0e12564d6e10c8d370613567714483bda2762cf0
aryawdy <aryawdy16@gmail.com>
2022-07-21 16:02:59 +0700

    login and register

---
+++ public/chat-app/src/utils/apiRouter.js
@@ -0,0 +1,9 @@
+const host = "http://localhost:3001";
+
+export const registerRoute = `${host}/api/auth/register`;
+
+export const loginRoute = `${host}/api/auth/login`;
+
+export const setAvatarRoute = `${host}/api/auth/set-avatar`;
+
+export const allUserRoute = `${host}/api/auth/all`;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoute);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("DB connection success");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// const server =
